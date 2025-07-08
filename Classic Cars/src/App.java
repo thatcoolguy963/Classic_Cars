26c56baa5b5c2e2b59925f952a1c9c9f3b11a142
thatcoolguy963 <bullsfan9197@gmail.com>
2025-07-08 14:27:47 -0400

    Update App.java

--- Classic Cars/src/App.java
+++ Classic Cars/src/App.java
@@ -1,7 +1,7 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
-import java.sql.PreparedStatement;
+import java.sql.Statement;
 
 public class App {
     public static void main(String[] args) throws Exception {
